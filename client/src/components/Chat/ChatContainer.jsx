ae80ac592bae40ab5c62a1409638c8a4dc83a03b
SidhantR <sidhantrc02@gmail.com>
2024-04-14 18:01:12 +0530

    Add message time and message status feature

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -1,11 +1,13 @@
 import { useStateProvider } from "@/context/StateContext";
+import { calculateTime } from "@/utils/CalculateTime";
 import React from "react";
+import MessageStatus from "../common/MessageStatus";
 
 const ChatContainer = () => {
   const [{ messages, currentChatUser, userInfo }] = useStateProvider();
   return (
     <div className="h-[80vh] w-full relative flex-grow overflow-auto custom-scrollbar ">
-      <div className="bg-chat-background bg-fixed h-full w-full opacity-5 fixed left-0 top-0 z-0   ">
+      <div className="bg-chat-background bg-fixed h-full w-full  fixed left-0 top-0 z-0">
         <div className="mx-10 my-6 relative bottom-0 z-40 left-0">
           <div className="flex w-full">
             <div className="flex flex-col justify-end w-full gap-1 overflow-auto ">
@@ -21,14 +23,24 @@ const ChatContainer = () => {
                   {console.log(currentChatUser, "message, ", message)}
                   {message.type === "text" && (
                     <div
-                      className={`text-white px-2 py-[5px] text-sm rounded-md flex gap-2 items-end max-w-[45%] 
+                      className={`text-white px-2 py-[5px] text-sm s rounded-md flex gap-2 items-end max-w-[45%] 
                     ${
                       message.senderId === currentChatUser.id
                         ? "bg-incoming-background"
-                        : "bg-outgoing-background "
+                        : "bg-outgoing-background"
                     }`}
                     >
                       <span className="break-all">{message.message} </span>
+                      <div className="flex gap-1 items-end">
+                        <span className="text-bubble-meta text-[11px] pt-1 min-w-fit">
+                          {calculateTime(message.createdAt)}
+                        </span>
+                        <span>
+                          {message.senderId === userInfo.id && 
+                          <MessageStatus messageStatus={message.messageStatus} />
+                          }
+                        </span>
+                      </div>
                     </div>
                   )}
                 </div>
